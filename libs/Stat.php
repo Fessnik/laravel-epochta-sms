12e1aa50fb24eee326ae468845f30ab2d0cfa36c
Fessnik <fesssnik@gmail.com>
2020-04-28 16:53:07 +0300

    Update Stat.php

--- libs/Stat.php
+++ libs/Stat.php
@@ -22,8 +22,8 @@ class Stat
     /*
     **	quick send sms. No list using, just 1 phone
     */
-    function sendSMS($sender, $text, $phone, $datetime, $sms_lifetime){
-        return $this->gateway->execCommad('sendSMS',array('sender' => $sender, 'text' => $text, 'phone' => $phone, 'datetime' => $datetime, 'sms_lifetime' => $sms_lifetime));
+     function sendSMS($attributes){
+        return $this->gateway->execCommad('sendSMS',$attributes);
     }
 
     /*
